d60ac11745fa0b39cbe3c586b92aa27ed154b7cb
RetroPipes <eric.ahnell@puttysoftware.com>
2025-06-27 11:05:01 -0400

    Move Fantastle Reboot into its own repo.

--- FantastleReboot/src/com/puttysoftware/fantastlereboot/objects/temporary/ArrowType.java
+++
@@ -1,5 +0,0 @@
-package com.puttysoftware.fantastlereboot.objects.temporary;
-
-public enum ArrowType {
-    WOODEN, SCORCH, FREEZE, SHADOW, VORTEX, CHARGE, SACRED, BEAUTY, LIQUID, POISON, PLASMA, MAGNET
-}
--- FantastleReboot/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.puttysoftware.fantastlereboot {
-    requires com.puttysoftware.diane;
-    requires com.puttysoftware.errorlogger;
-    requires com.puttysoftware.fileutils;
-    requires com.puttysoftware.integration;
-    requires com.puttysoftware.randomrange;
-    requires com.puttysoftware.updater;
-}
